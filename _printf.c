9557c3d6f3bfb40da5838d6ab5b20999bd8a21d3
Amina Murtala <murtalaameena@gmail.com>
2023-09-18 16:40:05 +0100

    flags

--- _printf.c
+++ _printf.c
@@ -1,62 +1,64 @@
 #include "main.h"
-
-void print_buffer(char buffer[], int *buff_ind);
+void print_bee(char bee[], int *buff_ind);
 
 /**
- * _printf - Printf function
- * @format: format
- * Return: Printed chars
+ * _printf - Printf
+ * @format: formt
+ * Return: Printed
  */
 int _printf(const char *format, ...)
 {
-	int k, meenah = 0, printed_chars = 0;
-	int flags, width, buff_ind, precision, width = 0;
+	int j, pointer = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
-	char buffer[BUFF_SIZE];
+	char bee[BUFF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
 	va_start(list, format);
 
-	for (k = 0; format && format[k] != '\0'; k++)
+	for (j = 0; format && format[j] != '\0'; j++)
 	{
-	if (format[k] != '%')
-	{
-		buffer[buff_ind++] = format[k];
-		if (buff_ind == BUFF_SIZE)
-			print_buffer(buffer, &buff_ind);
-		printed_chars++;
+		if (format[j] != '%')
+		{
+			bee[buff_ind++] = format[j];
+			if (buff_ind == BUFF_SIZE)
+				print_bee(bee, &buff_ind);
+			printed_chars++;
+		}
+		else
+		{
+			print_bee(bee, &buff_ind);
+			flags = get_flags(format, &j);
+			width = get_width(format, &j, list);
+			precision = get_precision(format, &j, list);
+			size = get_size(format, &j);
+			++j;
+			pointer = handle_print(format, &j, list, buffer,
+				flags, width, precision, size);
+			if (pointer == -1)
+				return (-1);
+			printed_chars += pointer;
+		}
 	}
-	else
-	{
-		print_buffer(buffer, buff_ind);
-		flags = get_flags(format, &k);
-		width = get_width(format, &k, list);
-		precision = get_precision(format, &k, list);
-		size = get_size(format, &k);
-		k++;
-		meenah = handle_print(format, &k, list,
-				buffer, flags, width, precision, size);
 
-		if (meenah == -1)
-			return (-1);
-		printed_chars += meenah;
-	}
-	}
+	print_bee(bee, &buff_ind);
 
-	print_buffer(buffer, &buff_ind);
 	va_end(list);
+
 	return (printed_chars);
 }
+
 /**
- * print_buffer - buffer
- * @buff_ind: index
- * @buffer: Arrays
+ * print_bee - Print
+ * @bee: Array
+ * @buff_ind: Index
  */
-void print_buffer(char buffer[], int *buff_ind)
+void print_bee(char bee[], int *buff_ind)
 {
-if (*buff_ind > 0)
-	write(1, &buffer[0], *buff_ind);
+	if (*buff_ind > 0)
+		write(1, &bee[0], *buff_ind);
+
 	*buff_ind = 0;
 }
--- main.h
+++ main.h
@@ -19,7 +19,19 @@ int putchr(char c);
 
 #define S_LONG 2
 #define S_SHORT 1
+/**
+ * typedef struct fmt fmt_t - Struct op
+ *
+ * @fmt: The format.
+ * @fm_t: The function associated.
+ */
+typedef struct fmt fmt_t;
 
+int _printf(const char *format, ...);
+int handle_print(const char *fmt, int *i,
+va_list list, char bee[], int flags, int width, int precision, int size);
+
+void print_bee(char bee[], int *buff_ind);
 int get_counter(const char *format, int *i);
 int get_meenah(const char *format, int *i, va_list list);
 int get_eze(const char *format, int *i, va_list list);
